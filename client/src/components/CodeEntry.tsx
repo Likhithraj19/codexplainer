4ab85fae1c2b5a4780b94b28b169f359f4147d23
Likhithraj19 <likhithrj@gmail.com>
2025-11-01 13:51:32 +0530

    Add CodeEntry component with Header and CodeExplainForm
    - Updated CodeEntry to include Header and CodeExplainForm components.
    - Created Header component for the application title.
    - Added CodeExplainForm component for user input with language selection and code submission.

--- client/src/components/CodeEntry.tsx
+++ client/src/components/CodeEntry.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './Header'
+import CodeExplainForm from './forms/CodeExplainForm'
 
 const CodeEntry = () => {
   return (
-    <div >CodeEntry</div>
+    <div className='min-h-screen flex flex-col items-center p-6' >
+        <Header/>
+        <CodeExplainForm/>
+    </div>
   )
 }
 
